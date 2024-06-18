da563546b9d4926448f1e251aaf027d15d9123c9
olili27 <timmolili27@gmail.com>
2024-06-18 19:39:38 +0300

    finish assignment

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+  let choices = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  let str = ""
+
+  while(l > 0) {
+    str += choices[Math.floor(Math.random() * choices.length)];
+    l--;
+  }
+  return str
 }
 
 // Do not change the code below.
